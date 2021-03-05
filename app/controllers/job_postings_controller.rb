a1681ccdedbebb9aa9d02aece22c24a30e3de2e7
github username <67525690+kirstybrews@users.noreply.github.com>
2021-03-04 21:09:06 -0600

    changed job_postings index action to include applications

--- app/controllers/job_postings_controller.rb
+++ app/controllers/job_postings_controller.rb
@@ -1,7 +1,9 @@
 class JobPostingsController < ApplicationController
     def index
         render json: JobPosting.all.to_json({
-            :except => [:updated_at, :created_at]
+            :include => {:applications => {
+                :include => [:user]
+            }}
           })
     end
 
