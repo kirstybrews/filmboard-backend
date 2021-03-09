3b10d32cc0c8ff0d8209a930d72790297a406a67
github username <67525690+kirstybrews@users.noreply.github.com>
2021-03-09 14:19:23 -0600

    added status to job postings backend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_174053) do
+ActiveRecord::Schema.define(version: 2021_03_09_181217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_03_08_174053) do
     t.string "project_type"
     t.string "compensation"
     t.boolean "need_gear"
+    t.string "status"
   end
 
   create_table "users", force: :cascade do |t|
